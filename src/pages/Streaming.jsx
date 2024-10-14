e288ef249a440ec4ccde1742d52cc615713bb459
Fernando Osmar Castro Martínez <jf3r123@gmail.com>
2024-10-14 15:46:49 -0600

    contruyendo la conxion al stream

--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect } from 'react'
 import { Row } from 'react-bootstrap'
 import { io } from 'socket.io-client'
+import video2 from '../assets/videoexample.mp4'
 
 function Streaming() {
 
+  const path_api =  process.env.REACT_APP_URL_API;
 
   const video = React.createRef(null);
   const canvas = React.createRef(null);
   const status = React.createRef(null);
+  const input = React.createRef(null);
+  const textarea = React.createRef(null);
 
   let context = null;
-  let socket = null;
+  let socket = io(path_api);;
 
+  const sendMessage = (e)=>{
+
+    console.log(input.current);
+    // socket.emit("chatExample",input.current.value);
+    context.current.drawImage(video.current,0,0,canvas.current.width,canvas.current.height);
+    var url = canvas.current.toDataURL("image/webp");
+
+    socket.emit("chatExample",url);
+
+  }
 
   useEffect(()=>{
 
@@ -19,22 +33,30 @@ function Streaming() {
     
     context = canvas.current.getContext('2d');
 
-    canvas.current.width = 512;
-    canvas.current.height = 384;
+    canvas.current.width = video.current.width;
+    canvas.current.height = video.current.height;
 
     context.width = canvas.current.width;
     context.height = canvas.current.height;
 
-    socket = io('http://localhost:3001');
+
+    socket.on("chatExample",(msg) =>{
+      console.log(textarea,"llego de retorno")
+      textarea.current.value = `${msg}`
+    })
   // eslint-disable-next-line
   },[]);
 
   return (
     <Row>
-      <video ref={video} src='' id='vide' style={{width:"800px",height:"600px"}} autoPlay={true}></video>
+      <video ref={video} src={video2} id='vide' style={{width:"600px",height:"400px"}} loop={true} autoPlay={true}></video>
       <canvas ref={canvas} id='preview'></canvas>
       <div ref={status} className='status'>Status</div>
-      <button>Start Stream</button>
+
+      <textarea ref={textarea}></textarea>
+
+      <input type="text" ref={input} />
+      <button onClick={sendMessage}>Start Stream</button>
     </Row>
   )
 }
