24f30c844db86e07a3cbcd8c6e309e1082d38bbb
Fernando Osmar Castro Martínez <jf3r123@gmail.com>
2024-10-16 15:54:36 -0600

    avances 10%

--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -1,62 +1,98 @@
 import React, { useEffect } from 'react'
 import { Row } from 'react-bootstrap'
 import { io } from 'socket.io-client'
-import video2 from '../assets/videoexample.mp4'
 
 function Streaming() {
 
   const path_api =  process.env.REACT_APP_URL_API;
 
-  const video = React.createRef(null);
-  const canvas = React.createRef(null);
+  const videoRef = React.createRef(null);
+  // const canvas = React.createRef(null);
   const status = React.createRef(null);
   const input = React.createRef(null);
-  const textarea = React.createRef(null);
+  // const textarea = React.createRef(null);
 
-  let context = null;
-  let socket = io(path_api);;
+  const socket = io(path_api);;
 
   const sendMessage = (e)=>{
 
     console.log(input.current);
     // socket.emit("chatExample",input.current.value);
-    context.current.drawImage(video.current,0,0,canvas.current.width,canvas.current.height);
-    var url = canvas.current.toDataURL("image/webp");
+    // context.current.drawImage(video.current,0,0,canvas.current.width,canvas.current.height);
+    // var url = canvas.current.toDataURL("image/webp");
 
-    socket.emit("chatExample",url);
+    // socket.emit("chatExample",url);
 
+  }
+
+  const startCapture = async()=>{
+    
+
+    try {
+
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true,
+      });
+
+      const mediaRecorder = new MediaRecorder(mediaStream);
+      
+      mediaRecorder.ondataavailable = (event) => {
+
+          console.log(event)
+          if (event.data.size > 0) {
+              socket.emit('video-stream', event.data);
+          }
+      };
+
+      mediaRecorder.start(100); // Enviar datos cada 100ms
+
+      mediaStream.getTracks().forEach(track => {
+          track.onended = () => {
+              mediaRecorder.stop();
+          };
+      });
+
+      status.current.innerHTML = "Status: Start stream..."
+
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+
+    // return captureStream;
   }
 
   useEffect(()=>{
 
-   console.log(canvas)
-    
-    context = canvas.current.getContext('2d');
+    socket.on('video-stream', (data) => {
+      const video = videoRef.current;
+      if (video) {
+        
+          const blob = new Blob([data.arrayBuffer], { type: 'video/webm; codecs=vp8, opus' });
+          const url = URL.createObjectURL(blob);
+          console.log(url)
+          video.src = url;
+          video.play();
+      }
+    });
 
-    canvas.current.width = video.current.width;
-    canvas.current.height = video.current.height;
+    return () => {
+        socket.off('video-stream');
+    };
+    // eslint-disable-next-line
+  },[])
 
-    context.width = canvas.current.width;
-    context.height = canvas.current.height;
-
-
-    socket.on("chatExample",(msg) =>{
-      console.log(textarea,"llego de retorno")
-      textarea.current.value = `${msg}`
-    })
-  // eslint-disable-next-line
-  },[]);
 
   return (
     <Row>
-      <video ref={video} src={video2} id='vide' style={{width:"600px",height:"400px"}} loop={true} autoPlay={true}></video>
-      <canvas ref={canvas} id='preview'></canvas>
-      <div ref={status} className='status'>Status</div>
+      <h1>Tramisión en Vivo</h1>
+      <video ref={videoRef} controls autoPlay></video>
+      <div ref={status} className='status'>Status : No Init</div>
 
-      <textarea ref={textarea}></textarea>
+      {/* <textarea ref={textarea}></textarea>
 
-      <input type="text" ref={input} />
-      <button onClick={sendMessage}>Start Stream</button>
+      <input type="text" ref={input} /> */}
+      <button onClick={startCapture}>Start Stream</button>
     </Row>
   )
 }
