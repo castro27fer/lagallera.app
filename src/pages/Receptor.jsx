697f1d7a626c4113ca7de9a0fc1382137a86ae2b
Fernando Osmar Castro Martínez <92816921+castro27fer@users.noreply.github.com>
2024-11-09 00:41:23 -0600

    conexión local con server de señalización exitoso

--- src/pages/Receptor.jsx
+++ src/pages/Receptor.jsx
@@ -1,34 +1,101 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap';
 import { io } from 'socket.io-client'
 
 function Receptor() {
 
-  const path_api =  process.env.REACT_APP_URL_API;
+  const API_SERVER =  process.env.REACT_APP_URL_API;
+  const STUN_SERVER = process.env.REACT_SERVER_STUN;
+
   const videoRef = React.createRef();
-  const socket = io(path_api);
+  let ICECandidates = [];
+
+  const peerConnection = new RTCPeerConnection({
+    iceServers: [
+      { 
+        urls: [
+          `stun:stun.l.google.com:19302`
+        ]
+      }
+    ],
+    iceTransportPolicy:"all"
+  });
+
+  let socket = null;
+
+  const connectStrem = async() =>{
+    
+    const offerOptions = { 
+      iceRestart:true,
+      offerToReceiveAudio: 0, 
+      offerToReceiveVideo: 1 
+    };
+    console.log("create offer")
+    const offer = await peerConnection.createOffer(offerOptions);
+    console.log("load local description")
+    await peerConnection.setLocalDescription(offer);
+    console.log("send offer")
+    socket.emit("offer",peerConnection.localDescription);
+  }
+
+  const loadCandidates = async()=>{
+    for(let i = 0; i<ICECandidates.length;i++){
+      await peerConnection.addIceCandidate(ICECandidates[i]);
+    }
+  }
+
+  const onCandidate = (event) => {
+    if(event.candidate){
+      socket.emit("candidate",{candidate:event.candidate,origin:"receptor"});
+    }
+  }
+
+  const onTrack = (e) =>{
+
+    console.log("onTrack",e);
+    videoRef.current.srcObject = e.streams[0];
+
+  };
+
+  const getAnswer = async(desc) => {
+    console.log("receive answer")
+    console.log("load remote description")
+    await peerConnection.setRemoteDescription(desc);
+    console.log("loader candidates");
+    await loadCandidates();
+
+    console.log("status",peerConnection.connectionState)
+  }
 
   useEffect(()=>{
 
-    socket.on('video-stream', (data) => {
-      // console.log("llego...")
-      videoRef.current.srcObject = data;
-      
-    });
+    peerConnection.onicecandidate = onCandidate;
+    peerConnection.ontrack = onTrack;
+    
+    if(socket === null){
+      socket = io(API_SERVER);
+      socket.on("getAnswer",(desc) =>getAnswer(desc));
+      socket.on("candidate",(candidate)=> ICECandidates.push(candidate.candidate));
+    }
+    
+    setInterval(()=>{
+      console.log(peerConnection.connectionState)
+    },1000)
 
-    // return () => {
-    //     socket.off('video-stream');
-    // };
-    // eslint-disable-next-line
+  
   },[])
+
+  
 
   return (<>
     <div>Receptor</div>
     <Row>
       <Col>
         <p>Recepción transmisión</p>
-        <video ref={videoRef}></video>
+        <video ref={videoRef} controls autoPlay></video>
         </Col>
+
+        <button onClick={connectStrem}>Conectar a la transmisión</button>
     </Row>
 
     </>)
--- src/pages/Streaming.jsx
+++ src/pages/Streaming.jsx
@@ -1,138 +1,161 @@
 import React, { useEffect, useState } from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
-// import { io } from 'socket.io-client'
+import { io } from 'socket.io-client'
 
 function Streaming() {
 
-  const path_api =  process.env.REACT_APP_URL_API;
-  const [play,set_play] = useState(false);
-  const [mediaStream,setMediaStream] = useState(null);
-  const pc1Local = new RTCPeerConnection();
-  const pc1Remote = new RTCPeerConnection();
+  const API_SERVER =  process.env.REACT_APP_URL_API;
+  const STUN_SERVER = process.env.REACT_SERVER_STUN;
 
   const videoRef = React.createRef(null);
-  const videoClientRef = React.createRef(null);
-
-  const canvas = React.createRef(null);
   const status = React.createRef(null);
-  const input = React.createRef(null);
-  const imgRef = React.createRef(null);
-  // let context = null;
-  // const textarea = React.createRef(null);
-  let preferredVideoCodecMimeType = 'video/VP8';
 
-  // const socket = io(path_api);
+  let ICECandidates = [];
+  let mediaStream = null;
+  // const [socket,setSocket] = useState(null)
+
+  const peerConnection = new RTCPeerConnection({
+    iceServers: [
+      { 
+        urls: [
+          `stun:stun.l.google.com:19302`
+        ]
+      }
+    ],
+    iceTransportPolicy:"all"
+  });
+
+  
+  let socket = null;
+  // let preferredVideoCodecMimeType = 'video/VP8';
 
   const startCapture = async()=>{
-    
-      navigator.mediaDevices.getDisplayMedia({
-        video: {
-          width:{
-            // min: 1024, //not soport
-            ideal: 600,
-            max:1920,
-          },
-          height:{
-            // min:576, //not soport
-            ideal:400,
-            max:1080,
-          },
-          frameRate:{
-            ideal:60,max:70,
-          },
+    navigator.mediaDevices.getDisplayMedia({
+      video: {
+        width:{
+          // min: 1024, //not soport
+          ideal: 600,
+          max:1920,
         },
-        audio: false,
-      })
-      .then(ms =>{
-        setMediaStream(()=>ms);
-        /*https://github.com/webrtc/
-        https://webrtc.org/getting-started/firebase-rtc-codelab?hl=es-419*/
+        height:{
+          // min:576, //not soport
+          ideal:400,
+          max:1080,
+        },
+        frameRate:{
+          ideal:60,max:70,
+        },
+      },
+      audio: false,
+    })
+    .then(ms => {
+      
+      mediaStream = ms;
+      // set_mediaStream(()=> ms); 
+      status.current.innerHTML = "Status: Start stream..."
+      videoRef.current.srcObject = mediaStream;
 
-      }).catch(err =>{
-        status.current.innerHTML =  `ERROR: ${err}`
-        console.log(`Error: ${err}`);
-      })
+      mediaStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, mediaStream);
+      });
+    })
+    .catch(err =>{
+      status.current.innerHTML =  `ERROR: ${err}`
+      console.log(`Error: ${err}`);
+    })
   }
 
-  const clientConnection = async(e)=>{
+// const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
+//   'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
 
-    const audioTracks = mediaStream.getAudioTracks();
-    const videoTracks = mediaStream.getVideoTracks();
+// const maybeSetCodecPreferences = (trackEvent) => {
+
+//   if (!supportsSetCodecPreferences) return;
+
+//   if (trackEvent.track.kind === 'video' && preferredVideoCodecMimeType) {
+//     const {codecs} = RTCRtpReceiver.getCapabilities('video');
+//     const selectedCodecIndex = codecs.findIndex(c => c.mimeType === preferredVideoCodecMimeType);
+//     const selectedCodec = codecs[selectedCodecIndex];
+//     codecs.splice(selectedCodecIndex, 1);
+//     codecs.unshift(selectedCodec);
+//     trackEvent.transceiver.setCodecPreferences(codecs);
+//   }
+// }
+
+  // const gotRemoteStream = (e, videoObject) => {
+  //   maybeSetCodecPreferences(e);
+  //   if (videoObject.srcObject !== e.streams[0]) {
+  //     videoObject.srcObject = e.streams[0];
+  //   }
+  // }
+
+
+  const createAnswer = async(desc) => {
+
+    try{
+      console.log("receive offer");
+      console.log("load remote description")
+      await peerConnection.setRemoteDescription(desc);
+      console.log("create Answer")
+      const answer = await peerConnection.createAnswer();
+      console.log("load local description")
+      await peerConnection.setLocalDescription(answer);
+      await loadCandidates();
+
+      console.log("status",peerConnection.connectionState)
+    }
+    catch(err){
+      console.log("error al cargar setRemoteDescription",err);
+    }
+    console.log("send answer");
+    socket.emit("answer",peerConnection.localDescription);
+  }
+
+  const onCandidate = (event) => {
+    if(event.candidate){
+      socket.emit("candidate",{candidate: event.candidate, origin:"emisor"});
+    }
+  }
+
+  const loadCandidates = async()=>{
+    console.log("loader candidates")
+    for(let i = 0; i<ICECandidates.length;i++){
+      await peerConnection.addIceCandidate(ICECandidates[i]);
+    }
+  }
+
+  // useEffect(()=>{
+  //   if(mediaStream !== null){
+  //     status.current.innerHTML = "Status: Start stream..."
+  //     videoRef.current.srcObject = mediaStream;
+
+  //     mediaStream.getTracks().forEach(track => {
+  //       console.log("add track")
+  //       peerConnection.addTrack(track, mediaStream);
+  //     });
+  //   }
     
-    if (audioTracks.length > 0) {
-      console.log(`Using audio device: ${audioTracks[0].label}`);
-    }
-    if (videoTracks.length > 0) {
-      console.log(`Using video device: ${videoTracks[0].label}`);
-    }
-   
-    //se cambia el evento ontrack el el que carga el TrackEvent en el objecto video enviado...
-    pc1Remote.ontrack = e => gotRemoteStream(e, videoClientRef.current);
 
-    console.log('pc1: created local and remote peer connection objects');
-
-    // pc2Local = new RTCPeerConnection();
-    // pc2Remote = new RTCPeerConnection();
-    // pc2Remote.ontrack = e => gotRemoteStream(e, video3);
-    // console.log('pc2: created local and remote peer connection objects');
-
-    //
-    mediaStream.getTracks().forEach(track => {
-      pc1Local.addTrack(track, mediaStream);
-      // pc2Local.addTrack(track, localStream);
-    });
-    await Promise.all([
-      negotiate(pc1Local, pc1Remote),
-      // negotiate(pc2Local, pc2Remote),
-    ]);
-}
-
-const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
-  'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
-
-const maybeSetCodecPreferences = (trackEvent) => {
-
-  if (!supportsSetCodecPreferences) return;
-
-  if (trackEvent.track.kind === 'video' && preferredVideoCodecMimeType) {
-    const {codecs} = RTCRtpReceiver.getCapabilities('video');
-    const selectedCodecIndex = codecs.findIndex(c => c.mimeType === preferredVideoCodecMimeType);
-    const selectedCodec = codecs[selectedCodecIndex];
-    codecs.splice(selectedCodecIndex, 1);
-    codecs.unshift(selectedCodec);
-    trackEvent.transceiver.setCodecPreferences(codecs);
-  }
-}
-
-const negotiate = async(localPc, remotePc) => {
-  localPc.onicecandidate = e => remotePc.addIceCandidate(e.candidate);
-  remotePc.onicecandidate = e => localPc.addIceCandidate(e.candidate);
-
-  await localPc.setLocalDescription();
-  await remotePc.setRemoteDescription(localPc.localDescription);
-  await remotePc.setLocalDescription();
-  await localPc.setRemoteDescription(remotePc.localDescription);
-}
-
-  const gotRemoteStream = (e, videoObject) => {
-    maybeSetCodecPreferences(e);
-    if (videoObject.srcObject !== e.streams[0]) {
-      videoObject.srcObject = e.streams[0];
-    }
-  }
+  // },[mediaStream])
 
   useEffect(()=>{
 
-    if(mediaStream !== null){
+    peerConnection.onicecandidate = onCandidate;
+    
+    if(socket === null){
 
-      videoRef.current.srcObject = mediaStream;
-      status.current.innerHTML = "Status: Start stream..."
-      console.log("estoy en bucle...")
-      
+      socket = io(API_SERVER);
+      socket.on("getOffer",(desc)=>createAnswer(desc));
+      socket.on("candidate",(candidate)=>ICECandidates.push(candidate.candidate));
     }
+    
+    setInterval(()=>{
+      console.log(peerConnection.connectionState)
+    },1000)
 
-  },[mediaStream])
+  },[])
 
+  
 
   return (<>
      
@@ -145,14 +168,7 @@ const negotiate = async(localPc, remotePc) => {
           <video ref={videoRef} controls autoPlay onError={(err) => console.log(err)}></video>
           <div ref={status} className='status'>Status : No Init</div>
           <button onClick={startCapture}>Start Stream</button>
-          <button onClick={clientConnection}>Conectar Cliente 1</button>
-        </Col>
-
-        <Col>
-          <p>Recepción</p>
-          <video ref={videoClientRef} playsInline autoPlay></video>
-          {/* <p>Recepción transmisión</p>
-          <img ref={imgRef} alt="" /> */}
+          {/* <button onClick={clientConnection}>Conectar Cliente 1</button> */}
         </Col>
 
       </Row>
